572bcb35b41cf892e6439534a443fdde0698d1fa
Braxton Madison <braxton.chs@gmail.com>
2018-09-06 14:08:14 -0400

    hello

---
+++ Desktop/Person5.java
@@ -0,0 +1,59 @@
+package edu.gatech.oad.antlab.person;
+//import java.util.Scanner;
+/**
+ *  A simple class for person 5
+ *  returns their name and a
+ *  modified string 
+ *  
+ *  @author Bob
+ *  @version 1.1
+ */
+public class Person5 {
+  /** Holds the persons real name */
+  private String name;
+  	/**
+	 * The constructor, takes in the persons
+	 * name
+	 * @param pname the person's real name
+	 */
+  public Person5(String pname) {
+    name = pname;
+  }
+  	/**
+	 * This method should take the string
+	 * input and return its characters rotated
+	 * 2 positions.
+	 * given "gtg123b" it should return
+	 * "g123bgt".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private String calc(String input) {
+      int length = input.length();
+      if (length < 3) {
+        return input;
+      } else {
+          return input.substring(2, length) + input.substring(0,2);
+      }
+	}
+	
+	/**
+	 * Return a string rep of this object
+	 * that varies with an input string
+	 *
+	 * @param input the varying string
+	 * @return the string representing the 
+	 *         object
+	 */
+	public String toString(String input) {
+	  return name + calc(input);
+	}
+    /**public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter characters: ");
+        String chars = scan.nextLine();
+        Person5 p = new Person5(chars);
+        System.out.println(p.toString(chars));
+    }**/
+}
